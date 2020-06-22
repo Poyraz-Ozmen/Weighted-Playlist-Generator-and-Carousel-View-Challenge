e54cce141ad59dfeeca9e72fd30d7ddb59009f6d
poyraz <poyrazozmen98@gmail.com>
2020-06-22 17:25:22 +0300

    added_dots_feature

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.getElementById("container");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-
+const dots = document.querySelectorAll(".dot");
+//console.log(dots);
 
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
 // image titles
-const images = ["https://cdn.pixabay.com/photo/2020/06/06/14/26/sunflower-5266745_960_720.jpg", "https://cdn.pixabay.com/photo/2020/06/14/09/27/seagull-5297122_960_720.jpg", "https://cdn.pixabay.com/photo/2015/12/01/19/35/portrait-1072696_960_720.jpg"];
+const images = [
+  "https://cdn.pixabay.com/photo/2020/06/06/14/26/sunflower-5266745_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/06/14/09/27/seagull-5297122_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2015/12/01/19/35/portrait-1072696_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/06/15/19/49/fuchs-5303221_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/06/20/12/32/tit-5320871__340.jpg",
+];
 
+let activeDotNum = 0;
+let clickedDotNum = 0;
 // Keep track of image
-let imageIndex = images.length;
 
+let imageIndex = 0;
 // Initially load image details into DOM
 loadImg(images[0]);
 
@@ -21,17 +30,20 @@ function loadImg(image) {
   cover.src = `${image}`;
 }
 
-
 // Previous image
 function previmage() {
   imageIndex--;
-
   if (imageIndex < 0) {
     imageIndex = images.length - 1;
   }
-
+  if (activeDotNum === 0) {
+    activeDotNum = 2;
+  } else {
+    activeDotNum--;
+  }
   loadImg(images[imageIndex]);
-
+  console.log("image_index: ", imageIndex);
+  console.log("activeDotNum: ", activeDotNum);
 }
 
 // Next image
@@ -41,15 +53,62 @@ function nextimage() {
   if (imageIndex > images.length - 1) {
     imageIndex = 0;
   }
-
+  if (activeDotNum === 2) {
+    activeDotNum = 0;
+  } else {
+    activeDotNum++;
+  }
   loadImg(images[imageIndex]);
-
+  console.log("image_index: ", imageIndex);
+  console.log("active: ", activeDotNum);
 }
-
 
 prevBtn.addEventListener("click", previmage);
 nextBtn.addEventListener("click", nextimage);
 
+dots.forEach((dot, idx) => {
+  //   number each dot according to array index
+  dot.setAttribute("data-num", idx);
 
+  //   add a click event listener to each dot
+  dot.addEventListener("click", (e) => {
+    clickedDotNum = e.target.dataset.num;
 
+    if (clickedDotNum === activeDotNum) {
+      //do nothing
+      return;
+    } else {
+      //if active is bigger
+      if (activeDotNum > clickedDotNum) {
+        if (activeDotNum === clickedDotNum + 1) {
+          previmage();
+        } else {
+          activeDotNum = activeDotNum - 2;
+          imageIndex = imageIndex - 2;
+          if (imageIndex < 0) {
+            imageIndex = images.length - 1;
+          }
 
+          loadImg(images[imageIndex]);
+          console.log("image_index: ", imageIndex);
+          console.log("activeDotNum: ", activeDotNum);
+        }
+      } else {
+        //clicked  is bigger
+        if (clickedDotNum === activeDotNum + 1) {
+          nextimage();
+        } else {
+          imageIndex = imageIndex + 2;
+
+          if (imageIndex > images.length - 1) {
+            imageIndex = 0;
+          }
+          activeDotNum = activeDotNum + 2;
+          loadImg(images[imageIndex]);
+          console.log("image_index: ", imageIndex);
+          console.log("active: ", activeDotNum);
+        }
+      }
+    }
+  });
+});
