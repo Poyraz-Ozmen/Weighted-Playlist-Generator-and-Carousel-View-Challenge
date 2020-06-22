7ecd0313b7a1c118713065b19f1613b1b446f533
poyraz <poyrazozmen98@gmail.com>
2020-06-22 13:03:47 +0300

    hard_coded_carousal_view

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+const container = document.getElementById("container");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// image titles
+const images = ["hey", "summer", "ukulele"];
+
+// Keep track of image
+let imageIndex = 2;
+
+// Initially load image details into DOM
+loadImg(images[imageIndex]);
+
+// Update image details
+function loadImg(image) {
+  cover.src = `images/${image}.jpg`;
+}
+
+
+// Previous image
+function previmage() {
+  imageIndex--;
+
+  if (imageIndex < 0) {
+    imageIndex = images.length - 1;
+  }
+
+  loadImg(images[imageIndex]);
+
+}
+
+// Next image
+function nextimage() {
+  imageIndex++;
+
+  if (imageIndex > images.length - 1) {
+    imageIndex = 0;
+  }
+
+  loadImg(images[imageIndex]);
+
+}
+
+
+prevBtn.addEventListener("click", previmage);
+nextBtn.addEventListener("click", nextimage);
+
+
+
 
